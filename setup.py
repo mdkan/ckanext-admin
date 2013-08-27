5214ee816ceea960e78d209627eb921af96cf1e0
Pinja Koskinen <pinja.koskinen@csc.fi>
2013-08-27 14:17:37 +0300

    Created extension

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-admin',
+	version=version,
+	description="System and usage reporting tools",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Pinja Koskinen',
+	author_email='pinja.koskinen@csc.fi',
+	url='http://www.csc.fi',
+	license='Beerware',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.admin'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.admin:PluginClass
+	""",
+)
