b01a2a3438c1481965db697c74bfcadb468ca4ac
Pinja Koskinen <pinja.koskinen@csc.fi>
2014-04-15 13:11:48 +0300

    Rewrote the initial version of reporting according to the new specs. Additional work is needed.

--- ckanext/admin/controllers.py
+++ ckanext/admin/controllers.py
@@ -3,6 +3,9 @@ Controller(s) for reporting in admin page
 '''
 
 import logging
+import sys
+
+import datetime
 
 from pylons import config
 
@@ -13,6 +16,15 @@ import ckanext.admin.report as report
 
 log = logging.getLogger(__name__)
 
+def datetime2date(datetime_):
+    return datetime.date(datetime_.year, datetime_.month, datetime_.day)
+
+def lastmonth(year, month):
+    if month == 1 or month == 01:
+        return (year-1, 12)
+    else:
+        return (year, month-1)
+
 class ReportController(AdminController):
     def report(self):
         '''
@@ -21,24 +33,36 @@ class ReportController(AdminController):
         '''
         # package info
         # Total packages
-        pkg_stats = report.PackageReport()
-        c.numpackages = pkg_stats.total_packages()
-        # Total of packages, where extra key=availability and value=direct_download + access_request
-        key = 'availability'
-        value = 'direct_download'
-        free = pkg_stats.total_packages_by_extra(key, value)
-        value = "access_request"
-        after_reg = pkg_stats.total_packages_by_extra(key, value)
-        c.openpackages = free + after_reg
-        # format to: d.dd %
         try:
-            c.popen = "{0:.2f}".format(float(c.openpackages) / float(c.numpackages) * 100) + ' %'
-        except ZeroDivisionError:
-            c.popen = "No packages"
+            pkg_stats = report.PackageReport()
+            c.numpackages = pkg_stats.total_packages()
+            # Total of packages, where extra key=availability and value=direct_download and license=cc0
+            c.cc0 = pkg_stats.total_packages_by_license("cc-zero")
+            # Total of packages, where extra key=availability and value=direct_download and license=cc-by
+            cc3by = pkg_stats.total_packages_by_license("cc-by")
+            cc4by = pkg_stats.total_packages_by_license("cc-by-4.0")
+            c.ccby = cc3by + cc4by
+            # Total of packages, where extra key=availability and value is not direct_download and 
+            # license is not notspecified
+            c.notfree = pkg_stats.total_nonfree_packages()
         
-        # user info
-        usr_stats = report.UserReport()
-        c.numusers = usr_stats.total_users()
+            c.pkg_per_month = pkg_stats.new_packages()
+            
+            # user info
+            usr_stats = report.UserReport()
+            
+            c.numusers = usr_stats.total_users()
+        
+            #date_ = datetime2date(datetime.date.today())
+            #c.date__0 = (date_.year, date_.month)
+            #c.usrmonth__0 = usr_stats.users_by_month(date_.year, date_.month)
+            #c.date__1 = lastmonth(date_.year, date_.month)
+            #c.usrmonth__1 = usr_stats.users_by_month(c.date__1[0], c.date__1[1])
+            #c.date__2 = lastmonth(c.date__1[0], c.date__1[1])
+            #c.usrmonth__2 = usr_stats.users_by_month(c.date__2[0], c.date__2[1])
+            c.users_per_month = usr_stats.users_by_month()
+        except TypeError:
+            log.debug(sys.exc_info()[0])
         
         return render('admin/report.html')
     
--- ckanext/admin/report.py
+++ ckanext/admin/report.py
@@ -5,11 +5,16 @@ Specific reports for authorities via tailored SQL queries.
 '''
 import logging
 
+import sqlalchemy as sa
+
 import ckan.model as model
 
 log = logging.getLogger(__name__)
 
 DATE_FORMAT = '%Y-%m-%d'
+
+def table(name):
+    return sa.Table(name, model.meta.metadata, autoload=True)
 
 class PackageReport(object):
     '''
@@ -46,6 +51,149 @@ class PackageReport(object):
                    count()
         return res
     
+    @classmethod
+    def total_packages_by_column(cls, column, value):
+        '''
+        Return total of packages, whose specific column and value
+        are given. Assuming state="active", type="dataset"
+        
+        :param
+        :return: count
+        '''
+        res = model.Session.query(model.Package.id).\
+                   filter(model.Package.state=="active").\
+                   filter(model.Package.type=="dataset").\
+                   filter(model.Package.column==value).\
+                   count()
+        return res
+    
+    @classmethod
+    def total_packages_by_license(cls, license):
+        '''
+        Return count of directly downloadable packages with
+        license as cc0
+        
+        :param license: license id
+        :return: count
+        '''
+        res = model.Session.query(model.Package.id).\
+                   filter(model.Package.state=="active").\
+                   filter(model.Package.type=="dataset").\
+                   filter(model.Package.license_id==license).\
+                   filter(model.PackageExtra.key=="availability").\
+                   filter(model.PackageExtra.value=="direct_download").\
+                   filter(model.Package.id==model.PackageExtra.package_id).\
+                   count()
+        return res
+    
+    @classmethod
+    def packages_by_license(cls, license):
+        '''
+        Return count of directly downloadable packages with
+        license as zz0
+        
+        :param license: license id
+        :return: list of ids as tuple
+        '''
+        res = model.Session.query(model.Package.id).\
+                   filter(model.Package.state=="active").\
+                   filter(model.Package.type=="dataset").\
+                   filter(model.Package.license_id==license).\
+                   filter(model.PackageExtra.key=="availability").\
+                   filter(model.PackageExtra.value=="direct_download").\
+                   filter(model.Package.id==model.PackageExtra.package_id).\
+                   all()
+        return res
+    
+    @classmethod
+    def total_nonfree_packages(cls):
+        '''
+        Return count of packages which are not freely downloadable
+        License must not be not specified
+        
+        :return: count
+        '''
+        res = model.Session.query(model.Package.id).\
+                   filter(model.Package.state=="active").\
+                   filter(model.Package.type=="dataset").\
+                   filter(model.Package.license_id!="notspecified").\
+                   filter(model.PackageExtra.key=="availability").\
+                   filter(model.PackageExtra.value!="direct_download").\
+                   filter(model.Package.id==model.PackageExtra.package_id).\
+                   count()
+        return res
+    
+    @classmethod
+    def nonfree_packages(cls):
+        '''
+        Return count of packages which are not freely downloadable
+        License must not be not specified
+        
+        :return: list of ids as tuple
+        '''
+        res = model.Session.query(model.Package.id).\
+                   filter(model.Package.state=="active").\
+                   filter(model.Package.type=="dataset").\
+                   filter(model.Package.license_id!="notspecified").\
+                   filter(model.PackageExtra.key=="availability").\
+                   filter(model.PackageExtra.value!="direct_download").\
+                   filter(model.Package.id==model.PackageExtra.package_id).\
+                   all()
+        return res
+    
+    @classmethod
+    def new_packages(cls):
+        package_revision = table('package_revision')
+        revision = table('revision')
+        
+        sel = """
+        select prid, min, extract(year from min) as y, extract(month from min) as m 
+from (select package_revision.id as prid, min(revision.timestamp) as min 
+from package_revision 
+join revision on revision.id = package_revision.revision_id 
+group by package_revision.id 
+order by min(revision.timestamp)) as tbl;
+        """
+                
+        res = model.Session.execute(sel).fetchall()
+        
+        nonfree = cls.nonfree_packages()
+        freecond = cls.packages_by_license('cc-by') + cls.packages_by_license('cc-by-4.0')
+        free = cls.packages_by_license('cc-zero')
+        
+        ret = {}
+        retfr = {}
+        retfrc = {}
+        retnfr = {}
+        
+        # TODO: find a better solution in sql, this looping is not good
+        # I really doubt this scales well with a mass of datasets, but for now
+        # we don't have that many
+        for prid, min, y, m in res:
+            key = unicode(int(y)) + "/" + unicode(int(m))
+            
+            if not key in ret:
+                ret[key] = int()
+            if not key in retfr:
+                retfr[key] = int()
+            if not key in retfrc:
+                retfrc[key] = int()
+            if not key in retnfr:
+                retnfr[key] = int()
+                
+            ret[key] += 1
+            
+            tprid = (prid,)
+            if tprid in free:
+                retfr[key] += 1
+            if tprid in freecond:
+                retfrc[key] += 1
+            if tprid in nonfree:
+                retnfr[key] += 1
+        
+        return {'total': ret, 'free': retfr, 'freecond': retfrc, 'nonfree': retnfr}
+        
+    
 class UserReport(object):
     '''
     Queries for user information
@@ -59,4 +207,26 @@ class UserReport(object):
         '''
         res = model.Session.query(model.User.id).count()
         return res
+    
+    @classmethod
+    def users_by_month(cls):
+        '''
+        New users by month
+        
+        :return: count
+        '''
+        user = table('user')
+        q = sa.select([sa.func.count('id'), sa.extract('year', user.c.created), \
+                        sa.extract('month', user.c.created)],\
+                        from_obj=[user]).\
+                        group_by('anon_1', 'anon_2').\
+                        order_by('anon_1', 'anon_2')
+        res = model.Session.execute(q).fetchall()
+        ret = {}
+        for count, anon_1, anon_2 in res:
+            key = unicode(int(anon_1)) + "/" + unicode(int(anon_2))
+            ret[key] = count
+
+        return ret
+        
 
