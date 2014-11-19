157c6b6e37e52d9bc05ccca19bfb7cff47abec06
Mikko Koho <mikko.koho@csc.fi>
2014-11-19 16:12:29 +0200

    Update version number to 0.2.0.

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.1'
+version = '0.2.0'
 
 setup(
-	name='ckanext-admin',
-	version=version,
-	description="Reporting tools for admins",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Pinja Koskinen',
-	author_email='pinja.koskinen@csc.fi',
-	url='http://www.csc.fi',
-	license='Beerware',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.admin'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    name='ckanext-admin',
+    version=version,
+    description="Reporting tools for admins",
+    long_description="""\
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Pinja Koskinen',
+    author_email='pinja.koskinen@csc.fi',
+    url='http://www.csc.fi',
+    license='Beerware',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.admin'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points=\
+    """
     [ckan.plugins]
-	# Add plugins here, eg
-	# myplugin=ckanext.admin:PluginClass
-	admin_reporting=ckanext.admin.plugin:Reporting
-	""",
+    # Add plugins here, eg
+    # myplugin=ckanext.admin:PluginClass
+    admin_reporting=ckanext.admin.plugin:Reporting
+    """,
 )
